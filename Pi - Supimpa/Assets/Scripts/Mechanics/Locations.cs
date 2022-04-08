ebbcccb1f08a5b6c29dae6d012c2980bc4070601
Gabriel <gabrielmaa@yahoo.com.br>
2022-04-07 22:45:53 -0300

    Mapa prototipo com nome das localizacoes commit

---
+++ Pi - Supimpa/Assets/Scripts/Mechanics/Locations.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Locations : MonoBehaviour
+{
+    public string locationName;
+
+    void Update()
+    {
+        
+    }
+}
--- Pi - Supimpa/Assets/Scripts/Mechanics/Player.cs
+++ Pi - Supimpa/Assets/Scripts/Mechanics/Player.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Player : MonoBehaviour
 {
     public float speed = 10;
     Rigidbody2D physics;
+    public TextMeshProUGUI locationText;
 
     void Start()
     {
@@ -18,5 +20,21 @@ public class Player : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
 
         physics.velocity = new Vector2(speed * horizontal, speed * vertical);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch (collision.tag)
+        {
+            case "Location":
+                if (!locationText.gameObject.activeInHierarchy)
+                {
+                    locationText.enabled = true;
+                }
+                locationText.text = collision.GetComponent<Locations>().locationName;
+                break;
+            default:
+                break;
+        }
     }
 }
