04f984b241fe13c906760c5823490e680a750963
Marcos_Andrade <mrcsandrade9@gmail.com>
2022-06-09 18:36:43 -0300

    Revert "Revert "O inimigo agr tem um range de detedção e consegue detectar os outros player (entretanto o outro player não consegue velo)""
    This reverts commit a8b8a83a3c9f7ec1043df4395109c1d041fec218.

--- Pi - Supimpa/Assets/Scripts/Mechanics/Enemies/EnemyAi.cs
+++ Pi - Supimpa/Assets/Scripts/Mechanics/Enemies/EnemyAi.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 using Pathfinding;
 public class EnemyAi : MonoBehaviour
 {
-    public Transform target;
+    Transform target;
 
     public float speed = 200f;
     public float nextWaipointDistance = 3;
+
+    public float lineOfSite;
 
     Path path;
     int currentWaypoint = 0;
@@ -16,20 +18,41 @@ public class EnemyAi : MonoBehaviour
     Seeker seeker;
     Rigidbody2D rb;
 
+    public Animator anim;
+
     void Start()
     {
         seeker = GetComponent<Seeker>();
         rb = GetComponent<Rigidbody2D>();
-
+       // anim = GameObject.Find("Enemy/Circle").GetComponent<Animator>();
         InvokeRepeating("UpdatePath", 0f, .5f);
     }
 
     private void UpdatePath()
     {
-        if (seeker.IsDone())
+        foreach (GameObject item in GameObject.FindGameObjectsWithTag("Player"))
         {
-            seeker.StartPath(rb.position, target.position, OnPathComplete);
+            target = item.transform;
+            float distanceFromPlayer = Vector2.Distance(target.position, transform.position);
+            if (distanceFromPlayer < lineOfSite)
+            {
+                if (seeker.IsDone())
+                {
+                    seeker.StartPath(rb.position, target.position, OnPathComplete);
+                }
+                anim.SetBool("Walking", true);
+            }
+            else
+            {
+                anim.SetBool("Walking", false);
+            }
         }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, lineOfSite);
     }
 
     void OnPathComplete(Path p)
@@ -43,11 +66,11 @@ public class EnemyAi : MonoBehaviour
 
     void FixedUpdate()
     {
-        if(path == null)
+        if (path == null)
         {
             return;
         }
-        if(currentWaypoint >= path.vectorPath.Count)
+        if (currentWaypoint >= path.vectorPath.Count)
         {
             reachedEndOfPath = true;
             return;
@@ -64,9 +87,20 @@ public class EnemyAi : MonoBehaviour
 
         float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
 
-        if(distance < nextWaipointDistance)
+        if (distance < nextWaipointDistance)
         {
             currentWaypoint++;
+        }
+
+        
+
+        if (direcition.x > 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        if (direcition.x < 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
         }
     }
 }
